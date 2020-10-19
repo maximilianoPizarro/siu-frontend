0174236bc21e462050e9c4f115f9a05c7cfd14d4
mpizarro <maximilianopi@buenosaires.gob.ar>
2020-10-19 08:20:54 -0300

    abm horarios terminado

--- src/main/webapp/app/entities/horario-manager/horario-management-update.component.ts
+++ src/main/webapp/app/entities/horario-manager/horario-management-update.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Curso } from 'app/core/curso/curso.model';
+import { CursoService } from 'app/core/curso/curso.service';
 
 import { Horario } from 'app/core/horario/horario.model';
 import { HorarioService } from 'app/core/horario/horario.service';
@@ -9,9 +12,12 @@ import { HorarioService } from 'app/core/horario/horario.service';
   selector: 'jhi-horario-mgmt-update',
   templateUrl: './horario-management-update.component.html',
 })
-export class HorarioManagementUpdateComponent implements OnInit {
+export class HorarioManagementUpdateComponent implements OnInit, OnDestroy {
   horario!: Horario;
   isSaving = false;
+  cursos: Curso[] | null = null;
+  cursosListSubscription?: any;
+  totalItems = 0;
 
   editForm = this.fb.group({
     idHorario: [],
@@ -21,15 +27,36 @@ export class HorarioManagementUpdateComponent implements OnInit {
     CursoIdCurso: [],
   });
 
-  constructor(private horarioService: HorarioService, private route: ActivatedRoute, private fb: FormBuilder) {}
+  constructor(
+    private horarioService: HorarioService,
+    private cursoService: CursoService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.loadAllCursos();
     this.route.data.subscribe(({ horario }) => {
       if (horario) {
         this.horario = horario;
         this.updateForm(horario);
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.cursosListSubscription.unsubscribe();
+  }
+
+  private loadAllCursos(): void {
+    this.cursosListSubscription = this.cursoService
+      .query()
+      .subscribe((res: HttpResponse<Curso[]>) => this.onSuccessCursos(res.body, res.headers));
+  }
+
+  private onSuccessCursos(cursos: Curso[] | null, headers: HttpHeaders): void {
+    this.totalItems = Number(headers.get('X-Total-Count'));
+    this.cursos = cursos;
   }
 
   previousState(): void {
@@ -65,6 +92,7 @@ export class HorarioManagementUpdateComponent implements OnInit {
     horario.dia = this.editForm.get(['dia'])!.value;
     horario.horarioInicio = this.editForm.get(['horarioInicio'])!.value;
     horario.horarioFin = this.editForm.get(['horarioFin'])!.value;
+    horario.CursoIdCurso = this.editForm.get(['CursoIdCurso'])!.value;
   }
 
   private onSaveSuccess(): void {
