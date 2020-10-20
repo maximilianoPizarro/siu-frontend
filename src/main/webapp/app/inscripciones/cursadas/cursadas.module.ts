20b92b711bece922af4b06bb8ac1d582c3daeb9c
Maximiliano Pizarro <maximiliano.pizarro.5@gmail.com>
2020-10-19 21:45:36 -0300

    Update cursadas.module.ts

--- src/main/webapp/app/inscripciones/cursadas/cursadas.module.ts
+++ src/main/webapp/app/inscripciones/cursadas/cursadas.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UnLaSiuSharedModule } from 'app/shared/shared.module';
-import { UnLaSiuInscripcionesModule } from '../inscripcion.module';
 import { CursadasManagementComponent } from './cursadas.component';
 import { cursadasManagementRoute } from './cursadas.route';
 
 @NgModule({
-  imports: [UnLaSiuInscripcionesModule, UnLaSiuSharedModule, RouterModule.forChild(cursadasManagementRoute)],
+  imports: [UnLaSiuSharedModule, RouterModule.forChild(cursadasManagementRoute)],
   declarations: [CursadasManagementComponent],
   entryComponents: [],
 })
