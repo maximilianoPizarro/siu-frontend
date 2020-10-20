01da25e59043db88120597337a1e46adbc85eeec
Maximiliano Pizarro <maximiliano.pizarro.5@gmail.com>
2020-10-19 21:46:34 -0300

    Update examenes.module.ts

--- src/main/webapp/app/inscripciones/examenes/examenes.module.ts
+++ src/main/webapp/app/inscripciones/examenes/examenes.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UnLaSiuSharedModule } from 'app/shared/shared.module';
-import { UnLaSiuInscripcionesModule } from '../inscripcion.module';
 import { ExamenesManagementComponent } from './examenes.component';
 import { examenesManagementRoute } from './examenes.route';
 
